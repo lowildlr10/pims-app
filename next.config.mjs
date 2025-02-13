b7333a98a5dd430946ba6c5be77ffc6099355c2e
Lowil Ray Delos Reyes <lowildlr10@gmail.com>
2025-02-13 20:51:00 +0800

    Updated the next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
@@ -7,6 +8,9 @@ const nextConfig = {
         hostname: 'localhost',
         pathname: '/**',
       },
+    ],
+    domains: [
+      'procsys-api.onrender.com'
     ],
   },
 };
